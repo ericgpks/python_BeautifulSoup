f51ee90d1a069c21299269ae8fdff2b9e55d69de
ericgpks <sq.wacbc5.nyc@gmail.com>
2024-04-07 09:58:44 +0900

    initial commit

---
+++ ticket.py
@@ -0,0 +1,21 @@
+# coding: UTF-8
+from base64 import b64decode
+import boto3
+import os
+import json
+import urllib.request
+import requests
+from bs4 import BeautifulSoup
+
+
+def lambda_handler(event, context):
+    url = "xxx"
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    show_ttl = soup.select(".show_list_ttl")
+    if len(show_ttl) != 0:
+        WEB_HOOK_URL = os["WEB_HOOK_URL"]
+        requests.post(WEB_HOOK_URL, data=json.dumps({
+            # メッセージ
+            "text": "<!channel> message",
+        }))
